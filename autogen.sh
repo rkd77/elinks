9a5e25b31e46c2e218c27f5ef689243c41d82132
Jonas Fonseca <fonseca@diku.dk>
2005-09-27 23:19:52 +0200

    Remove last automake trace and improve acinclude.m4 header
    It seems that running autoheader is unnecessary so put your hands in the
    air and wave goodbye to automake.
    
    Mention that acinclude.m4 is generated by autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo acinclude.m4...
-echo "dnl This is automatically generated from m4/ files! Do not modify!" > acinclude.m4
-cat config/m4/*.m4 >> acinclude.m4
+(
+	echo "dnl Automatically generated from config/m4/ files by autogen.sh!"
+	echo "dnl Do not modify!"
+	cat config/m4/*.m4
+) > acinclude.m4
 
 echo aclocal...
 aclocal
-
-echo autoheader...
-autoheader
 
 echo autoconf...
 autoconf
