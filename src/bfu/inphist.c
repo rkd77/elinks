093800d6fbf5df70b499bb13eea61a9172fb14ea
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2005-12-16 23:59:04 +0000

    Only set match the first time since we are only interested in the common substring at the start, which will be the same for all matches.

--- src/bfu/inphist.c
+++ src/bfu/inphist.c
@@ -124,10 +124,10 @@ do_tab_compl_unambiguous(struct dialog_data *dlg_data, struct list_head *history
 
 		if (!match) {
 			longest_common_match = strlen(entry->data);
+			match = entry->data;
 		} else if (cur_len < longest_common_match) {
 			longest_common_match = cur_len;
 		}
-		match = entry->data;
 	}
 
 	if (!match) return;
