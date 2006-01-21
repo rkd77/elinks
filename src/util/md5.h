rkd77/elinks

commit 206037eaa497aa9e369e5594b3a58c3c9b4b7237

Handle the logic for util/{md5,sha1} in the Makefile

src/util/Makefile
@@ -5,12 +5,25 @@ include $(top_builddir)/Makefile.config
 # Usage $(call not,$(CONFIG_FOO))
 not = $(if $(findstring yes,$(1)),no,yes)
 
-OBJS-$(CONFIG_DEBUG)		 += memdebug.o
-OBJS-$(CONFIG_MD5)		 += md5.o
-OBJS-$(CONFIG_CSS)		 += scanner.o
-OBJS-$(CONFIG_DOM)		 += scanner.o
-OBJS-$(CONFIG_SHA1)		 += sha1.o
-OBJS-$(call not,$(CONFIG_SMALL)) += fastfind.o
+OBJS-$(call not,$(CONFIG_SMALL))	 += fastfind.o
+OBJS-$(CONFIG_CSS)			 += scanner.o
+OBJS-$(CONFIG_DEBUG)			 += memdebug.o
+OBJS-$(CONFIG_DOM)			 += scanner.o
+OBJS-$(CONFIG_MD5)			 += md5.o
+
+# Use own MD5 implementation if testing libc or there's no OpenSSL
+# (either the real thing or GNUTLS compat wrappers).
+OBJS-$(CONFIG_OWN_LIBC)			 += md5.o
+ifeq ($(CONFIG_GNUTLS_OPENSSL_COMPAT),no)
+OBJS-$(call not,$(CONFIG_OPENSSL))	 += md5.o
+endif
+
+ifeq ($(CONFIG_BITTORRENT),yes)
+# BitTorrent is the only user. Compile in SHA1 if testing libc or
+# there is no OpenSSL. The GNUTLS OpenSSL wrappers doesn't have it.
+OBJS-$(CONFIG_OWN_LIBC)			 += sha1.o
+OBJS-$(call not,$(CONFIG_OPENSSL))	 += sha1.o
+endif
 
 OBJS = \
  base64.o \

Makefile.config.in
@@ -131,7 +131,6 @@ CONFIG_LEDS = @CONFIG_LEDS@
 CONFIG_MAILCAP = @CONFIG_MAILCAP@
 CONFIG_MANUAL = @CONFIG_MANUAL@
 CONFIG_MARKS = @CONFIG_MARKS@
-CONFIG_MD5 = @CONFIG_MD5@
 CONFIG_MIMETYPES = @CONFIG_MIMETYPES@
 CONFIG_MOUSE = @CONFIG_MOUSE@
 CONFIG_NNTP = @CONFIG_NNTP@
@@ -151,7 +150,6 @@ CONFIG_SCRIPTING_PERL = @CONFIG_SCRIPTING_PERL@
 CONFIG_SCRIPTING_PYTHON = @CONFIG_SCRIPTING_PYTHON@
 CONFIG_SCRIPTING_RUBY = @CONFIG_SCRIPTING_RUBY@
 CONFIG_SCRIPTING_SPIDERMONKEY = @CONFIG_SCRIPTING_SPIDERMONKEY@
-CONFIG_SHA1 = @CONFIG_SHA1@
 CONFIG_SMALL = @CONFIG_SMALL@
 CONFIG_SMB = @CONFIG_SMB@
 CONFIG_SPIDERMONKEY = @CONFIG_SPIDERMONKEY@

configure.in
@@ -1224,29 +1224,6 @@ EL_ARG_ENABLE(CONFIG_OWN_LIBC, own-libc, [Own libc stubs],
 EL_ARG_ENABLE(CONFIG_SMALL, small, [Small binary],
 	      [  --enable-small          reduce binary size as far as possible (but see the bottom of doc/small.txt!)])
 
-if test "$CONFIG_OPENSSL" != yes &&
-   test "$CONFIG_GNUTLS_OPENSSL_COMPAT" != yes ||
-   test "$CONFIG_OWN_LIBC" = yes;
-then
-	AC_MSG_CHECKING(for built-in MD5 support)
-	AC_MSG_RESULT(yes)
-	EL_CONFIG(CONFIG_MD5, [Built-in MD5])
-fi
-
-AC_SUBST(CONFIG_MD5)
-
-if test "$CONFIG_BITTORRENT" = yes; then
-	if test "$CONFIG_OPENSSL" != yes ||
-	   test "$CONFIG_OWN_LIBC" = yes;
-	then
-		AC_MSG_CHECKING(for built-in SHA1 support)
-		AC_MSG_RESULT(yes)
-		EL_CONFIG(CONFIG_SHA1, [Built-in SHA1])
-	fi
-fi
-
-AC_SUBST(CONFIG_SHA1)
-
 AC_ARG_ENABLE(weehoofooboomookerchoo,
               [
     Also check out the features.conf file for more information about features!

commit 384a2d66e5bea985a8ddfe5b285f7ab7c397d14b

Fix inclusion of header files after CONFIG_{MD5,SHA1} were removed
Fixes: 206037eaa497aa9e369e5594b3a58c3c9b4b7237

src/util/Makefile
@@ -1,6 +1,8 @@
 top_builddir=../..
 include $(top_builddir)/Makefile.config
 
+INCLUDES += $(GNUTLS_CFLAGS) $(OPENSSL_CFLAGS)
+
 # Reverse a CONFIG_* string
 # Usage $(call not,$(CONFIG_FOO))
 not = $(if $(findstring yes,$(1)),no,yes)
@@ -9,7 +11,6 @@ OBJS-$(call not,$(CONFIG_SMALL))	 += fastfind.o
 OBJS-$(CONFIG_CSS)			 += scanner.o
 OBJS-$(CONFIG_DEBUG)			 += memdebug.o
 OBJS-$(CONFIG_DOM)			 += scanner.o
-OBJS-$(CONFIG_MD5)			 += md5.o
 
 # Use own MD5 implementation if testing libc or there's no OpenSSL
 # (either the real thing or GNUTLS compat wrappers).

src/util/md5.h
@@ -3,12 +3,14 @@
 
 /* Optionally MD5 support can depend on external implementation when linking
  * against a SSL library that supports it. */
-#ifndef CONFIG_MD5
-#if defined(CONFIG_OPENSSL)
+#if defined(CONFIG_OWN_LIBC)
+#define CONFIG_MD5 1
+#elif defined(CONFIG_OPENSSL)
 #include <openssl/md5.h>
 #elif defined(CONFIG_GNUTLS_OPENSSL_COMPAT)
 #include <gnutls/openssl.h>
-#endif
+#else
+#define CONFIG_MD5 1
 #endif
 
 /* GNU TLS doesn't define this */

src/util/sha1.h
@@ -36,10 +36,12 @@
 
 /* Optionally SHA1 support can depend on external implementation when linking
  * against a SSL library that supports it. */
-#ifndef CONFIG_SHA1
-#if defined(CONFIG_OPENSSL)
+#if defined(CONFIG_OWN_LIBC)
+#define CONFIG_SHA1 1
+#elif defined(CONFIG_OPENSSL)
 #include <openssl/sha.h>
-#endif
+#else
+#define CONFIG_SHA1 1
 #endif
 
 #ifndef SHA_DIGEST_LENGTH