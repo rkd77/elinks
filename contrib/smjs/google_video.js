e9d12e91285618b8aaccf55826b1dbf91643efee
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2006-01-25 15:50:51 +0000

    Reflow some logic in google_video.js.

--- contrib/smjs/google_video.js
+++ contrib/smjs/google_video.js
@@ -2,13 +2,12 @@
  * from the front page or the search page, and the video will automatically
  * be loaded. */
 function load_google_video(cached) {
-	if (cached.uri.match(/^http:\/\/video.google.com\/videoplay/)) {
-		var re;
-		re = /(<object data="\/googleplayer.swf\?videoUrl=)(.*?)(\&)/;
-		var uri = cached.content.match(re)[2];
+	if (!cached.uri.match(/^http:\/\/video.google.com\/videoplay/))
+		return true;
+	var re = /(<object data="\/googleplayer.swf\?videoUrl=)(.*?)(\&)/;
+	var uri = cached.content.match(re)[2];
 
-		if (uri) elinks.location = unescape(uri);
-	}
+	if (uri) elinks.location = unescape(uri);
 
 	return true;
 }
