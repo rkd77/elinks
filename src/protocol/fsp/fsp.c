rkd77/elinks

commit af2cc87054a14cbff59e183163418334a4915f66

FSP: display_entry() cleanup, and mark directories by an ending / in lists.

src/protocol/fsp/fsp.c
@@ -84,13 +84,17 @@ compare(FSP_RDENTRY *a, FSP_RDENTRY *b)
 static void
 display_entry(FSP_RDENTRY *fentry, unsigned char dircolor[])
 {
-	printf("%10d\t<a href=\"%s%s\">", fentry->size,
-	       fentry->name, fentry->type == FSP_RDTYPE_DIR ? "/" : "");
-	if (fentry->type == FSP_RDTYPE_DIR && *dircolor)
-		printf("<font color=\"%s\"><b>", dircolor);
-	printf("%s", fentry->name);
-	if (fentry->type == FSP_RDTYPE_DIR && *dircolor)
-		printf("</b></font>");
+	printf("%10d\t<a href=\"%s", fentry->size, fentry->name);
+	if (fentry->type == FSP_RDTYPE_DIR) {
+		printf("/\">");
+		if (*dircolor)
+			printf("<font color=\"%s\"><b>", dircolor);
+		printf("%s/", fentry->name);
+		if (*dircolor)
+			printf("</b></font>");
+	} else {
+		printf("/\">%s", fentry->name);
+	}
 	puts("</a>");
 }
 

commit 7d1ff4fbf1344c75d0f252c0b180c5a190a6dce4

Only add ending slash to directory URLs
Not even to directory names. We don't do that for file: or ftp: protocol.

Fixes: af2cc87054a14cbff59e183163418334a4915f66

src/protocol/fsp/fsp.c
@@ -89,11 +89,11 @@ display_entry(FSP_RDENTRY *fentry, unsigned char dircolor[])
 		printf("/\">");
 		if (*dircolor)
 			printf("<font color=\"%s\"><b>", dircolor);
-		printf("%s/", fentry->name);
+		printf("%s", fentry->name);
 		if (*dircolor)
 			printf("</b></font>");
 	} else {
-		printf("/\">%s", fentry->name);
+		printf("\">%s", fentry->name);
 	}
 	puts("</a>");
 }