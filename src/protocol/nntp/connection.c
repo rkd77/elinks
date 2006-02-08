rkd77/elinks

commit 1735f96a871f7d4c5f54ef14f8dc7390c601db7f

FTP, NNTP: Normalize the cache entry before setting the connection state
... so it is normalized when the download callbacks are called.

src/protocol/nntp/connection.c
@@ -217,8 +217,6 @@ nntp_end_request(struct connection *conn, enum connection_state state)
 		return;
 	}
 
-	set_connection_state(conn, state);
-
 	if (conn->state == S_OK) {
 		if (conn->cached) {
 			normalize_cache_entry(conn->cached, conn->from);
@@ -228,6 +226,7 @@ nntp_end_request(struct connection *conn, enum connection_state state)
 		 * grabing the keepalive connection will be able to go on. */
 	}
 
+	set_connection_state(conn, state);
 	add_keepalive_connection(conn, NNTP_KEEPALIVE_TIMEOUT, nntp_quit);
 }
 

src/protocol/ftp/ftp.c
@@ -1347,11 +1347,10 @@ out_of_mem:
 static void
 ftp_end_request(struct connection *conn, enum connection_state state)
 {
-	set_connection_state(conn, state);
-
-	if (conn->state == S_OK && conn->cached) {
+	if (state == S_OK && conn->cached) {
 		normalize_cache_entry(conn->cached, conn->from);
 	}
 
+	set_connection_state(conn, state);
 	add_keepalive_connection(conn, FTP_KEEPALIVE_TIMEOUT, NULL);
 }

commit 4bfd40ec107fdda668ca1e32ba476ceee7e5156d

NNTP: Use state instead of conn->state, since it hasn't been updated yet
Fixes: 1735f96a871f7d4c5f54ef14f8dc7390c601db7f

src/protocol/nntp/connection.c
@@ -217,11 +217,11 @@ nntp_end_request(struct connection *conn, enum connection_state state)
 		return;
 	}
 
-	if (conn->state == S_OK) {
+	if (state == S_OK) {
 		if (conn->cached) {
 			normalize_cache_entry(conn->cached, conn->from);
 		}
-	} else if (conn->state == S_OUT_OF_MEM) {
+	} else if (state == S_OUT_OF_MEM) {
 		/* FIXME: Clear the socket buffers before ending so the one
 		 * grabing the keepalive connection will be able to go on. */
 	}