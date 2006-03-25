dd93d35f4ca10b230927c566807224cc64a0a91d
Witold Filipczyk <witekfl@poczta.onet.pl>
2006-03-25 11:40:09 +0100

    Missing lzma.h

---
+++ src/encoding/lzma.h
@@ -0,0 +1,12 @@
+#ifndef EL__ENCODING_LZMA_H
+#define EL__ENCODING_LZMA_H
+
+#include "encoding/encoding.h"
+
+#ifdef CONFIG_LZMA
+extern struct decoding_backend lzma_decoding_backend;
+#else
+#define lzma_decoding_backend dummy_decoding_backend
+#endif
+
+#endif
