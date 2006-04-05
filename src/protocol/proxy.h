8b77bc66171e5b69f2185fd35ce2d8fa901b9a67
Witold Filipczyk <witekfl@poczta.onet.pl>
2006-04-05 16:27:16 +0200

    enum connection_state instead of int to satisfy tcc

--- src/protocol/proxy.h
+++ src/protocol/proxy.h
@@ -1,13 +1,14 @@
 #ifndef EL__PROTOCOL_PROXY_H
 #define EL__PROTOCOL_PROXY_H
 
+enum connection_state;
 struct uri;
 
 /* Checks if the passed URI has been configured to go through a proxy. The
  * fragment is removed from the returned URI. */
 /* If @connection_state is non-NULL it will be set to indicate what error
  * occured if the function returns NULL. */
-struct uri *get_proxy_uri(struct uri *uri, int *connection_state);
+struct uri *get_proxy_uri(struct uri *uri, enum connection_state *connection_state);
 
 /* ``Translates'' the passed URI into the URI being proxied. If it is not a
  * proxy:// URI it will return the URI with the fragment removed.  */
