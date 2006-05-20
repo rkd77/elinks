eee3ecdbbc3678f9970ea8d37d7044c4152375e6
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2006-05-20 12:11:27 +0000

    BitTorrent: Remove superfluous check and assignment
    unchoke_bittorrent_peer checks and clears peer->remote.choked,
    so do not do so in the caller.

--- src/protocol/bittorrent/connection.c
+++ src/protocol/bittorrent/connection.c
@@ -114,10 +114,7 @@ update_bittorrent_connection_state(struct connection *conn)
 			continue;
 
 		if (min_uploads < max_uploads) {
-			if (peer->remote.choked)
-				unchoke_bittorrent_peer(peer);
-
-			peer->remote.choked = 0;
+			unchoke_bittorrent_peer(peer);
 
 			/* Uninterested peers are not counted as uploads. */
 			if (peer->remote.interested)
