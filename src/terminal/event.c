9e30ee631ced843f26a264c351cfa1716e7e1941
Kalle Olavi Niemitalo <kon@iki.fi>
2006-08-05 13:30:00 +0300

    terminal: Do not call toupper with potentially out-of-range values.
    For instance, if Ctrl-F1 were pressed and src/terminal/kbd.c supported it,
    then toupper(KBD_F1) would be called, resulting in undefined behaviour.
    src/terminal/kbd.c does not support such combinations yet, but it is
    safest to fix the bug already.

--- src/terminal/event.c
+++ src/terminal/event.c
@@ -256,7 +256,7 @@ handle_interlink_event(struct terminal *term, struct term_event *ev)
 
 		reset_timer();
 
-		if (check_kbd_modifier(ev, KBD_MOD_CTRL) && toupper(key) == 'L') {
+		if (check_kbd_modifier(ev, KBD_MOD_CTRL) && (key == 'l' || key == 'L')) {
 			redraw_terminal_cls(term);
 			break;
 
