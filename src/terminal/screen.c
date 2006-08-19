13dc5f01ea252e638e5c8b92396e8ba180dd16e3
Witold Filipczyk <witekfl@poczta.onet.pl>
2006-08-19 23:57:50 +0200

    Note about efficiency. I know that normal 16 color mode is broken (menu). Maybe someone knows how to fix it.

--- src/terminal/screen.c
+++ src/terminal/screen.c
@@ -431,6 +431,9 @@ struct screen_state {
 #endif
 };
 
+/* FIXME: This is inefficient. When CONFIG_TRUE_COLOR is defined even in 16 color mode
+ * all 6 bytes are copied or compared. */
+
 #if defined(CONFIG_TRUE_COLOR)
 #define compare_color(a, b)	((a)[0] == (b)[0] && (a)[1] == (b)[1] && (a)[2] == (b)[2] \
 				&& (a)[3] == (b)[3] && (a)[4] == (b)[4] && (a)[5] == (b)[5])
