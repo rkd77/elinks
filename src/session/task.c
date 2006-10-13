3e5b4b7adf9fea9ed06781456f69de2e58984cc6
Laurent MONIN <zas@norz.org>
2006-10-13 15:08:52 +0200

    Fix whitespace goof reported by Miciah on IRC, introduced in 947c1730a61900fd3443696182b6692f2669bf56.

--- src/session/task.c
+++ src/session/task.c
@@ -540,7 +540,8 @@ loading_callback(struct download *download, struct session *ses)
 
 	if (is_in_result_state(download->state) && download->state != S_OK) {
 		print_error_dialog(ses, download->state,
-				   download->conn ? download->conn->uri : NULL,				   	   download->pri);
+				   download->conn ? download->conn->uri : NULL,
+				   download->pri);
 		if (d == DO_MOVE_ABORT) reload(ses, CACHE_MODE_NORMAL);
 	}
 
