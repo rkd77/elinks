701e7e3e648fd86e0f152c9bda31796fe2a9be4e
Kalle Olavi Niemitalo <kon@iki.fi>
2007-03-10 16:56:16 +0200

    cgi: Avoid assertion failure when the POST data is empty.

--- src/protocol/file/cgi.c
+++ src/protocol/file/cgi.c
@@ -120,8 +120,14 @@ send_post_data(struct connection *conn)
 		add_bytes_to_string(&data, buffer, n);
 
 
-	write_to_socket(conn->data_socket, data.source, data.length,
-			S_SENT, close_pipe_and_read);
+	/* If we're submitting a form whose controls do not have
+	 * names, then the POST has a Content-Type but empty data,
+	 * and an assertion would fail in write_to_socket.  */
+	if (data.length)
+		write_to_socket(conn->data_socket, data.source, data.length,
+				S_SENT, close_pipe_and_read);
+	else
+		close_pipe_and_read(conn->data_socket);
 
 	done_string(&data);
 #undef POST_BUFFER_SIZE
