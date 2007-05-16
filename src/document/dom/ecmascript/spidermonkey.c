6da89f7acc8dfef8d942f99dbbc65183acfa81fd
Witold Filipczyk <witekfl@poczta.onet.pl>
2007-05-16 21:26:07 +0200

    DOM: Definition of attribute callback.

--- src/document/dom/ecmascript/spidermonkey.c
+++ src/document/dom/ecmascript/spidermonkey.c
@@ -47,3 +47,43 @@ done_dom_node_ecmascript_obj(struct dom_node *node)
 	JS_SetPrivate(ctx, obj, NULL);
 	node->ecmascript_obj = NULL;
 }
+
+JSObject *
+make_new_object(JSContext *ctx, struct dom_node *node)
+{
+#if 0
+	JSObject *obj = create_new_object[node->data.element.type](ctx);
+
+	if (obj) {
+		JS_SetPrivate(ctx, obj, node);
+		node->ecmascript_obj = obj;
+	}
+	return obj;
+#endif
+	return NULL;
+}
+
+void
+dom_add_attribute(struct dom_node *root, struct dom_node *node, struct dom_string *attr, struct dom_string *value)
+{
+	JSContext *ctx = root->data.document.ecmascript_ctx;
+	JSObject *obj = node->ecmascript_obj;
+	JSString *string;
+	jsval vp;
+	unsigned char tmp;
+
+	if (!obj) {
+		obj = make_new_object(ctx, node);
+		if (!obj) return;
+	}
+	if (value) {
+		string = JS_NewString(ctx, value->string, value->length);
+		vp = STRING_TO_JSVAL(string);
+	} else {
+		vp = JSVAL_NULL;
+	}
+	tmp = attr->length;
+	attr->string[attr->length] = '\0';
+	JS_SetProperty(ctx, obj, attr->string, &vp);
+	attr->string[attr->length] = tmp;
+}
--- src/document/dom/ecmascript/spidermonkey.h
+++ src/document/dom/ecmascript/spidermonkey.h
@@ -4,6 +4,9 @@
 #include "document/dom/ecmascript/spidermonkey/props.h"
 
 struct dom_node;
+struct dom_string;
+
 void done_dom_node_ecmascript_obj(struct dom_node *node);
+void dom_add_attribute(struct dom_node *root, struct dom_node *parent, struct dom_string *attr, struct dom_string *value);
 
 #endif
