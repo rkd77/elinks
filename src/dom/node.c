a29ef8600c8bd580abe90bc7d1b86b3a34ab7dc0
Witold Filipczyk <witekfl@poczta.onet.pl>
2007-05-19 20:43:41 +0200

    DOM: SEE in not supported yet.

--- src/dom/node.c
+++ src/dom/node.c
@@ -464,9 +464,11 @@ done_dom_node(struct dom_node *node)
 		}
  	}
 #ifdef CONFIG_ECMASCRIPT
+#ifdef CONFIG_SPIDERMONKEY
 	if (node->ecmascript_obj) {
 		done_dom_node_ecmascript_obj(node);
 	}
+#endif
 #endif
 	if (node->type == DOM_NODE_ELEMENT && node->data.element.html_data) {
 		done_dom_node_html_data(node);
