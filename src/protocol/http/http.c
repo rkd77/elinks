eb1fe8ee5a5f4f449228c5a0fdbb03a16d684998
Witold Filipczyk <witekfl@poczta.onet.pl>
2007-06-09 11:40:13 +0200

    http: use off_t for the chunk size.

--- src/protocol/http/http.c
+++ src/protocol/http/http.c
@@ -1193,11 +1193,11 @@ read_chunked_http_data(struct connection *conn, struct read_buffer *rb)
 
 			if (l) {
 				unsigned char *de;
-				int n = 0;
+				off_t n = 0;
 
 				if (l != -1) {
 					errno = 0;
-					n = strtol(rb->data, (char **) &de, 16);
+					n = strtoll(rb->data, (char **) &de, 16);
 					if (errno || !*de) {
 						return -1;
 					}
