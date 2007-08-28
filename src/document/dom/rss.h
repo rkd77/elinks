556ae45b006c48a25fcc566792d6933f7a8e1d0b
Petr Baudis <pasky@ucw.cz>
2007-08-28 17:31:43 +0200

    document/dom: Split RSS renderer to rss.*

---
+++ src/document/dom/rss.h
@@ -0,0 +1,13 @@
+
+#ifndef EL__DOCUMENT_DOM_RSS_H
+#define EL__DOCUMENT_DOM_RSS_H
+
+#include "dom/stack.h"
+
+
+#define RSS_CONFIG_FLAGS \
+	(DOM_CONFIG_NORMALIZE_WHITESPACE | DOM_CONFIG_NORMALIZE_CHARACTERS)
+
+extern struct dom_stack_context_info dom_rss_renderer_context_info;
+
+#endif
