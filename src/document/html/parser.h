2a90b0e4b39b2db440476f515a304317a9814802
Petr Baudis <pasky@ucw.cz>
2007-08-29 14:08:34 +0200

    document/html/parser.h: Whitespace

--- src/document/html/parser.h
+++ src/document/html/parser.h
@@ -97,7 +97,6 @@ struct par_attrib {
 
 
 /* Major lifetime of the parser for the whole document */
-
 struct html_context *init_html_parser(struct uri *uri,
 		       struct document_options *options,
 		       unsigned char *start, unsigned char *end,
@@ -113,7 +112,6 @@ void done_html_parser(struct html_context *html_context);
 /* Set up parser "sub-state"; the parser may be invoked on various
  * segments of the document repeatedly (in practice it is used for
  * re-parsing table contents); is_root is set for the "main" invocation. */
-
 void *init_html_parser_state(struct html_context *html_context,
                              int is_root, int align, int margin, int width);
 void done_html_parser_state(struct html_context *html_context,
@@ -122,18 +120,15 @@ void done_html_parser_state(struct html_context *html_context,
 
 /* Do the parsing job, calling callbacks specified at init_html_parser()
  * time as required. */
-
 void parse_html(unsigned char *html, unsigned char *eof, struct part *part,
 		unsigned char *head, struct html_context *html_context);
 
 
 /* This examines imgmap in the given document and produces a BFU-ish menu
  * for it. */
-
-int
-get_image_map(unsigned char *head, unsigned char *pos, unsigned char *eof,
-	      struct menu_item **menu, struct memory_list **ml, struct uri *uri,
-	      struct document_options *options, unsigned char *target_base,
-	      int to, int def, int hdef);
+int get_image_map(unsigned char *head, unsigned char *pos, unsigned char *eof,
+                  struct menu_item **menu, struct memory_list **ml,
+		  struct uri *uri, struct document_options *options,
+		  unsigned char *target_base, int to, int def, int hdef);
 
 #endif
