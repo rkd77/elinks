9831a2c8052085129add537057b0f0517bbda9fb
Kalle Olavi Niemitalo <kon@iki.fi>
2007-09-09 19:15:57 +0300

    Add tests for cwd-relative file URIs.
    Test 6 currently fails.

--- src/protocol/test/uri-test.c
+++ src/protocol/test/uri-test.c
@@ -17,6 +17,11 @@ main(int argc, char **argv)
 	 * taking arguments like --normalize-uri=<arg> etc. */
 	if (argc == 2) {
 		fprintf(stdout, "%s\n", normalize_uri(NULL, argv[1]));
+	} else if (argc == 3) {
+		struct uri *translated = get_translated_uri(argv[1], argv[2]);
+		if (translated == NULL)
+			return EXIT_FAILURE;
+		fprintf(stdout, "%s\n", struri(translated));
 	}
 	return 0;
 }
