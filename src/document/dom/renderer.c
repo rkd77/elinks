rkd77/elinks

commit 09cf904814f47cf178e4539acf9d422c9b33cf0f

Backport Pasky's changes concerning text_style-related stuff.
It partially includes changes made in following commits:

document/html: struct text_attrib_style -> struct text_style
commit    e1339412068f97be1aa5f91871ba7ebeedbdb5f

document: struct format_attr -> struct text_style_format
commit    070d335796c3e71c5feff8813e681ef4534ee016

document: Unify text style -> screen attribute handling
commit    b66d2bec674f59016ca0708c20249421914b1d2b

document: Move text_style-related stuff to dedicated format.*
commit    db9431465fd77658cfe804947ab336158c1974bd

src/document/dom/renderer.c
@@ -1038,7 +1038,7 @@ render_dom_document(struct cache_entry *cached, struct document *document,
 
 	init_dom_renderer(&renderer, document, buffer, convert_table);
 
-	document->bgcolor = document->options.default_bg;
+	document->bgcolor = document->options.default_style.bg;
 #ifdef CONFIG_UTF8
 	document->options.utf8 = is_cp_utf8(document->options.cp);
 #endif /* CONFIG_UTF8 */

src/document/html/renderer.c
@@ -340,39 +340,19 @@ static inline struct screen_char *
 get_format_screen_char(struct html_context *html_context,
                        enum link_state link_state)
 {
-	static struct text_attrib_style ta_cache = { -1, 0x0, 0x0 };
+	static struct text_style ta_cache = { -1, 0x0, 0x0 };
 	static struct screen_char schar_cache;
 
 	if (memcmp(&ta_cache, &format.style, sizeof(ta_cache))) {
 		copy_struct(&ta_cache, &format.style);
-
-		schar_cache.attr = 0;
-		if (format.style.attr) {
-			if (format.style.attr & AT_UNDERLINE) {
-				schar_cache.attr |= SCREEN_ATTR_UNDERLINE;
-			}
-
-			if (format.style.attr & AT_BOLD) {
-				schar_cache.attr |= SCREEN_ATTR_BOLD;
-			}
-
-			if (format.style.attr & AT_ITALIC) {
-				schar_cache.attr |= SCREEN_ATTR_ITALIC;
-			}
-
-			if (format.style.attr & AT_GRAPHICS) {
-				schar_cache.attr |= SCREEN_ATTR_FRAME;
-			}
-		}
+		struct text_style final_style = format.style;
 
 		if (link_state != LINK_STATE_NONE
 		    && html_context->options->underline_links) {
-			schar_cache.attr |= SCREEN_ATTR_UNDERLINE;
+			final_style.attr |= AT_UNDERLINE;
 		}
 
-		set_screen_char_color(&schar_cache, format.style.bg, format.style.fg,
-				      html_context->options->color_flags,
-				      html_context->options->color_mode);
+		get_screen_char_template(&schar_cache, html_context->options, final_style);
 	}
 
 	if (!!(schar_cache.attr & SCREEN_ATTR_UNSEARCHABLE)

src/document/plain/renderer.c
@@ -14,6 +14,7 @@
 #include "config/options.h"
 #include "document/docdata.h"
 #include "document/document.h"
+#include "document/format.h"
 #include "document/options.h"
 #include "document/plain/renderer.h"
 #include "document/renderer.h"
@@ -92,7 +93,7 @@ add_document_link(struct document *document, unsigned char *uri, int length,
 	link->npoints = length;
 	link->type = LINK_HYPERTEXT;
 	link->where = uri;
-	link->color.background = document->options.default_bg;
+	link->color.background = document->options.default_style.bg;
 	link->color.foreground = document->options.default_link;
 	link->number = document->nlinks;
 
@@ -213,7 +214,7 @@ print_document_link(struct plain_renderer *renderer, int lineno,
 
 	line[link_end] = saved_char;
 
-	new_link->color.background = doc_opts->default_bg;
+	new_link->color.background = doc_opts->default_style.bg;
 
 	set_term_color(&template, &new_link->color,
 		       doc_opts->color_flags, doc_opts->color_mode);
@@ -478,14 +479,7 @@ next:
 static void
 init_template(struct screen_char *template, struct document_options *options)
 {
-	color_T background = options->default_bg;
-	color_T foreground = options->default_fg;
-	struct color_pair colors = INIT_COLOR_PAIR(background, foreground);
-
-	template->attr = 0;
-	template->data = ' ';
-	set_term_color(template, &colors,
-		       options->color_flags, options->color_mode);
+	get_screen_char_template(template, options, options->default_style);
 }
 
 static struct node *
@@ -643,7 +637,7 @@ render_plain_document(struct cache_entry *cached, struct document *document,
 	renderer.max_width = document->options.wrap ? document->options.box.width
 						    : INT_MAX;
 
-	document->bgcolor = document->options.default_bg;
+	document->bgcolor = document->options.default_style.bg;
 	document->width = 0;
 #ifdef CONFIG_UTF8
 	document->options.utf8 = is_cp_utf8(document->options.cp);

src/document/Makefile
@@ -6,6 +6,6 @@ SUBDIRS-$(CONFIG_DOM)	+= dom
 
 SUBDIRS = html plain
 
-OBJS = docdata.o document.o forms.o options.o refresh.o renderer.o
+OBJS = docdata.o document.o format.o forms.o options.o refresh.o renderer.o
 
 include $(top_srcdir)/Makefile.lib

src/document/css/apply.c
@@ -21,6 +21,7 @@
 #include "document/css/property.h"
 #include "document/css/scanner.h"
 #include "document/css/stylesheet.h"
+#include "document/format.h"
 #include "document/html/parser/parse.h"
 #include "document/options.h"
 #include "util/align.h"

src/document/css/property.h
@@ -2,7 +2,8 @@
 #ifndef EL__DOCUMENT_CSS_PROPERTY_H
 #define EL__DOCUMENT_CSS_PROPERTY_H
 
-#include "document/html/parser.h"
+#include "document/format.h"
+#include "util/align.h"
 #include "util/color.h"
 #include "util/lists.h"
 
@@ -48,7 +49,7 @@ struct css_property {
 			CSS_DISP_BLOCK,
 		} display;
 		struct {
-			enum format_attr add, rem;
+			enum text_style_format add, rem;
 		} font_attribute;
 		enum format_align text_align;
 		/* TODO:

src/document/css/stylesheet.h
@@ -2,6 +2,7 @@
 #ifndef EL__DOCUMENT_CSS_STYLESHEET_H
 #define EL__DOCUMENT_CSS_STYLESHEET_H
 
+#include "protocol/uri.h"
 #include "util/lists.h"
 
 /* #define DEBUG_CSS */

src/document/format.c
@@ -1,0 +1,46 @@
+/** Format attributes utilities
+ * @file */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "elinks.h"
+
+#include "document/format.h"
+#include "document/options.h"
+#include "terminal/draw.h"
+#include "util/color.h"
+
+
+void
+get_screen_char_template(struct screen_char *template,
+			 struct document_options *options,
+			 struct text_style style)
+{
+	template->attr = 0;
+	template->data = ' ';
+
+	if (style.attr) {
+		if (style.attr & AT_UNDERLINE) {
+			template->attr |= SCREEN_ATTR_UNDERLINE;
+		}
+
+		if (style.attr & AT_BOLD) {
+			template->attr |= SCREEN_ATTR_BOLD;
+		}
+
+		if (style.attr & AT_ITALIC) {
+			template->attr |= SCREEN_ATTR_ITALIC;
+		}
+
+		if (style.attr & AT_GRAPHICS) {
+			template->attr |= SCREEN_ATTR_FRAME;
+		}
+	}
+
+	{
+		struct color_pair colors = INIT_COLOR_PAIR(style.bg, style.fg);
+		set_term_color(template, &colors, options->color_flags, options->color_mode);
+	}
+}

commit 7b6cb249ed964d7800cda526fa240676ddd80230

Fix compilation using --enable-html-highlight.
It was broken by commit 09cf904814f47cf178e4539acf9d422c9b33cf0f.
Reported by witekfl.

src/document/dom/renderer.c
@@ -115,8 +115,8 @@ init_dom_renderer(struct dom_renderer *renderer, struct document *document,
 
 	for (type = 0; type < DOM_NODES; type++) {
 		struct screen_char *template = &renderer->styles[type];
-		color_T background = document->options.default_bg;
-		color_T foreground = document->options.default_fg;
+		color_T background = document->options.default_style.bg;
+		color_T foreground = document->options.default_style.fg;
 		enum screen_char_attr attr = 0;
 		static int i_want_struct_module_for_dom;
 
@@ -418,7 +418,7 @@ add_dom_link(struct dom_renderer *renderer, unsigned char *string, int length,
 	link->npoints = length;
 	link->type = LINK_HYPERTEXT;
 	link->where = where;
-	link->color.background = document->options.default_bg;
+	link->color.background = document->options.default_style.bg;
 	link->color.foreground = fgcolor;
 	link->number = document->nlinks;
 