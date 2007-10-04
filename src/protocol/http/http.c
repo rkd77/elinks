rkd77/elinks

commit b3845a799803dc88cedbc99be603518486f76bf9

bug 534: Fixed (?)

src/protocol/http/http.c
@@ -1010,13 +1010,18 @@ decompress_data(struct connection *conn, unsigned char *data, int len,
 	off_t *length_of_block;
 	unsigned char *output = NULL;
 
-	length_of_block = (http->length == LEN_CHUNKED ? &http->chunk_remaining
-						       : &http->length);
-
 #define BIG_READ 65536
-	if (!*length_of_block) {
-		/* Going to finish this decoding bussiness. */
-		state = FINISHING;
+	if (http->length == LEN_CHUNKED) {
+		if (http->chunk_remaining == CHUNK_ZERO_SIZE)
+			state = FINISHING;
+		else
+			length_of_block = &http->chunk_remaining;
+	} else {
+		length_of_block = &http->length;
+		if (!*length_of_block) {
+			/* Going to finish this decoding bussiness. */
+			state = FINISHING;
+		}
 	}
 
 	if (conn->content_encoding == ENCODING_NONE) {
@@ -1221,8 +1226,8 @@ read_chunked_http_data(struct connection *conn, struct read_buffer *rb)
 			int len;
 			int zero = (http->chunk_remaining == CHUNK_ZERO_SIZE);
 
-			if (zero) http->chunk_remaining = 0;
-			len = http->chunk_remaining;
+			if (zero) len = 0;
+			else len = http->chunk_remaining;
 
 			if (http->chunk_remaining > INT_MAX)
 				int_lower_bound(&len, rb->length);

commit 7dd9ef674a0c033729cf76bc8235333878027f04

This version of ELinks crashed on a normal chunked data. This bug was introduced in commit b3845a799803dc88cedbc99be603518486f76bf9.

src/protocol/http/http.c
@@ -1010,14 +1010,15 @@ decompress_data(struct connection *conn, unsigned char *data, int len,
 	off_t *length_of_block;
 	unsigned char *output = NULL;
 
+
+	length_of_block = (http->length == LEN_CHUNKED ? &http->chunk_remaining
+						       : &http->length);
+
 #define BIG_READ 65536
 	if (http->length == LEN_CHUNKED) {
 		if (http->chunk_remaining == CHUNK_ZERO_SIZE)
 			state = FINISHING;
-		else
-			length_of_block = &http->chunk_remaining;
 	} else {
-		length_of_block = &http->length;
 		if (!*length_of_block) {
 			/* Going to finish this decoding bussiness. */
 			state = FINISHING;