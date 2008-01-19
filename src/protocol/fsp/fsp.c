481041ae48b656ad96d6650740b789b45df9a6b7
Kalle Olavi Niemitalo <kon@iki.fi>
2008-01-19 10:55:11 +0200

    Bug 939: Documented the fix.
    The fix itself is in the parent commit.
    (cherry picked from commit 4c390589ea28eeba020357fd89841b0fc2be33b3,
     rewriting the NEWS entry because the bug also occurred on Debian)

--- src/protocol/fsp/fsp.c
+++ src/protocol/fsp/fsp.c
@@ -90,6 +90,13 @@ compare(FSP_RDENTRY *a, FSP_RDENTRY *b)
 static void
 sort_and_display_entries(FSP_DIR *dir)
 {
+	/* fsp_readdir_native in fsplib 0.9 and earlier requires
+	 * the third parameter to point to a non-null pointer
+	 * even though it does not dereference that pointer
+	 * and overwrites it with another one anyway.
+	 * http://sourceforge.net/tracker/index.php?func=detail&aid=1875210&group_id=93841&atid=605738
+	 * Work around the bug by using non-null &tmp.
+	 * Nothing will actually read or write tmp.  */
 	FSP_RDENTRY fentry, tmp, *table = NULL;
 	FSP_RDENTRY *fresult = &tmp;
 	int size = 0;
@@ -157,6 +164,8 @@ fsp_directory(FSP_SESSION *ses, struct uri *uri)
 	if (get_opt_bool("protocol.fsp.sort")) {
 		sort_and_display_entries(dir);
 	} else {
+		/* &tmp works around a bug in fsplib 0.9 or earlier.
+		 * See sort_and_display_entries for details.  */
 		FSP_RDENTRY fentry, tmp;
 		FSP_RDENTRY *fresult = &tmp;
 		unsigned char dircolor[8];
