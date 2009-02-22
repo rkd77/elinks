codecandiescom/elinks-history

commit b2d51c75ff0d6c52a4f6a2761801beb641cba3a2

Add search-toggle-regex in the edit map to toggle document.browse.search.regex in the type-ahead input field. Bind to alt-r by default, like in EMACS.

src/viewer/text/search.c
@@ -1,5 +1,5 @@
 /* Searching in the HTML document */
-/* $Id: search.c,v 1.265 2004/08/09 08:38:07 miciah Exp $ */
+/* $Id: search.c,v 1.266 2004/08/09 08:47:34 miciah Exp $ */
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE /* XXX: we _WANT_ strcasestr() ! */
@@ -1225,6 +1225,18 @@ text_typeahead_handler(struct input_line *line, int action)
 		case ACT_EDIT_NEXT_ITEM:
 			find_next(ses, doc_view, 1);
 			break;
+
+		case ACT_EDIT_SEARCH_TOGGLE_REGEX: {
+			struct option *opt =
+				get_opt_rec(config_options,
+					    "document.browse.search.regex");
+
+			opt->value.number = (opt->value.number + 1)
+					    % (opt->max + 1);
+			opt->flags |= OPT_TOUCHED;
+		}
+		report_errors = 1;
+		/* Fall thru */
 
 		default:
 			search_for_do(ses, buffer, direction, report_errors);

src/config/kbdbind.c
@@ -1,5 +1,5 @@
 /* Keybinding implementation */
-/* $Id: kbdbind.c,v 1.255 2004/07/28 12:25:00 jonas Exp $ */
+/* $Id: kbdbind.c,v 1.256 2004/08/09 08:47:34 miciah Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -569,6 +569,7 @@ static struct strtonum edit_action_table[EDIT_ACTIONS + 1] = {
 	{ "previous-item", ACT_EDIT_PREVIOUS_ITEM, DACT(N_("Move to the previous item")) },
 	{ "redraw", ACT_EDIT_REDRAW, DACT(N_("Redraw the terminal")) },
 	{ "right", ACT_EDIT_RIGHT, DACT(N_("Move the cursor right")) },
+	{ "search-toggle-regex", ACT_EDIT_SEARCH_TOGGLE_REGEX, DACT(N_("Toggle regex matching (type-ahead searching)")) },
 	{ "up", ACT_EDIT_UP, DACT(N_("Move cursor upwards")) },
 
 	{ NULL, 0, NULL }
@@ -782,6 +783,7 @@ static struct default_kb default_edit_keymap[] = {
 	{ 'H',		 KBD_CTRL,	ACT_EDIT_BACKSPACE },
 	{ 'K',		 KBD_CTRL,	ACT_EDIT_KILL_TO_EOL },
 	{ 'L',		 KBD_CTRL,	ACT_EDIT_REDRAW },
+	{ 'r',		 KBD_ALT,	ACT_EDIT_SEARCH_TOGGLE_REGEX },
 	{ 'R',		 KBD_CTRL,	ACT_EDIT_AUTO_COMPLETE_UNAMBIGUOUS },
 	{ 'T',		 KBD_CTRL,	ACT_EDIT_OPEN_EXTERNAL },
 	{ 'U',		 KBD_CTRL,	ACT_EDIT_KILL_TO_BOL },

src/config/kbdbind.h
@@ -1,4 +1,4 @@
-/* $Id: kbdbind.h,v 1.135 2004/07/27 22:41:00 jonas Exp $ */
+/* $Id: kbdbind.h,v 1.136 2004/08/09 08:47:34 miciah Exp $ */
 
 #ifndef EL__CONFIG_KBDBIND_H
 #define EL__CONFIG_KBDBIND_H
@@ -171,6 +171,7 @@ enum edit_action {
 	ACT_EDIT_PREVIOUS_ITEM,
 	ACT_EDIT_REDRAW,
 	ACT_EDIT_RIGHT,
+	ACT_EDIT_SEARCH_TOGGLE_REGEX,
 	ACT_EDIT_UP,
 
 	EDIT_ACTIONS

rkd77/elinks

commit b81821a21c6ca1c959386dca11f87382e7b03d69

Fix crash on search-toggle-regex when RE disabled
Check the return value of get_opt_rec on "document.browse.search.regex"
before dereferencing it.  The option is not there if regular expression
support is disabled at build time.

This commit fixes a bug introduced in commit
b2d51c75ff0d6c52a4f6a2761801beb641cba3a2.

src/viewer/text/search.c
@@ -1423,9 +1423,11 @@ text_typeahead_handler(struct input_line *line, int action_id)
 				get_opt_rec(config_options,
 					    "document.browse.search.regex");
 
-			opt->value.number = (opt->value.number + 1)
-					    % (opt->max + 1);
-			option_changed(ses, opt);
+			if (opt) {
+				opt->value.number = (opt->value.number + 1)
+						    % (opt->max + 1);
+				option_changed(ses, opt);
+			}
 		}
 		/* Fall thru */
 