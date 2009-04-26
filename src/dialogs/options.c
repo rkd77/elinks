8acf2e36c3542b6b9640f42c70f13c995acedc64
Kalle Olavi Niemitalo <kon@iki.fi>
2009-04-26 13:23:10 +0300

    big dialogs: Always show help in terminal options dialog
    We now have scrollable dialog boxes so the height check is no longer
    necessary.

--- src/dialogs/options.c
+++ src/dialogs/options.c
@@ -170,27 +170,21 @@ terminal_options(struct terminal *term, void *xxx, struct session *ses)
 	size_t help_textlen = 0;
 	size_t add_size = TERM_OPTION_VALUE_SIZE;
 
-	/* XXX: we don't display help text when terminal height is too low,
-	 * because then user can't change values.
-	 * This should be dropped when we'll have scrollable dialog boxes.
-	 * --Zas */
-	if (term->height > 30) {
-		snprintf(help_text, sizeof(help_text) - 3 /* 2 '\n' + 1 '\0' */,
-			 _("The environmental variable TERM is set to '%s'.\n"
-			"\n"
-			"ELinks maintains separate sets of values for these options\n"
-			"and chooses the appropriate set based on the value of TERM.\n"
-			"This allows you to configure the settings appropriately for\n"
-			"each terminal in which you run ELinks.", term),
-			 term->spec->name);
+	snprintf(help_text, sizeof(help_text) - 3 /* 2 '\n' + 1 '\0' */,
+		 _("The environmental variable TERM is set to '%s'.\n"
+		"\n"
+		"ELinks maintains separate sets of values for these options\n"
+		"and chooses the appropriate set based on the value of TERM.\n"
+		"This allows you to configure the settings appropriately for\n"
+		"each terminal in which you run ELinks.", term),
+		 term->spec->name);
 
-		help_textlen = strlen(help_text);
+	help_textlen = strlen(help_text);
 
-		/* Two newlines are needed to get a blank line between the help text and
-		 * the first group of widgets. */
-		help_text[help_textlen++] = '\n';
-		help_text[help_textlen++] = '\n';
-	}
+	/* Two newlines are needed to get a blank line between the help text and
+	 * the first group of widgets. */
+	help_text[help_textlen++] = '\n';
+	help_text[help_textlen++] = '\n';
 
 	help_text[help_textlen++] = '\0';
 
