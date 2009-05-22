76377d9714f2be22f84597173c9770e62513699a
Miciah Dashiel Butler Masters <miciah.masters@gmail.com>
2009-05-22 23:10:54 +0000

    Bug 765: use ses_load to load old tab's document
    Yet another valiant wack at the beast.  This one violates abstractions
    a little less deeply, so maybe it will work better.
    
    The last attempt caused a crash when a tab was cloned after the tab's
    loading had been aborted.

--- src/session/task.h
+++ src/session/task.h
@@ -19,6 +19,10 @@ struct link_def {
 
 void abort_preloading(struct session *, int);
 
+void ses_load(struct session *ses, struct uri *uri, unsigned char *target_frame,
+              struct location *target_location, enum cache_mode cache_mode,
+              enum task_type task_type);
+
 void ses_goto(struct session *, struct uri *, unsigned char *,
 	      struct location *, enum cache_mode, enum task_type, int);
 struct view_state *ses_forward(struct session *, int);
