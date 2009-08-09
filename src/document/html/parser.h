87eac68dc304e48d2b012efdf66eb76c64cf84de
Kalle Olavi Niemitalo <kon@iki.fi>
2009-08-09 17:46:21 +0300

    Fix "parameter has incomplete type" on GCC 3.4.6
    Fix this GCC 3.4.6 warning, which becomes an error
    if configure --enable-debug adds -Werror to CFLAGS:
    
          [CC]   src/document/css/apply.o
    In file included from /home/Kalle/src/elinks-0.12/src/document/html/internal.h:6,
                     from /home/Kalle/src/elinks-0.12/src/document/css/apply.c:35:
    /home/Kalle/src/elinks-0.12/src/document/html/parser.h:149: warning: parameter has incomplete type
    In file included from /home/Kalle/src/elinks-0.12/src/document/css/apply.c:35:
    /home/Kalle/src/elinks-0.12/src/document/html/internal.h:125: warning: parameter has incomplete type
    
    Even without this warning, "enum html_special_type;"
    would not be standard C89.
    (cherry picked from elinks-0.12 commit c9f487cdf44c97948559d882f7b59e37b51cd7af)
    
    Conflicts:
    	src/document/html/parser.h: 0.13.GIT had more #includes already.

--- src/document/html/parser.h
+++ src/document/html/parser.h
@@ -4,6 +4,7 @@
 
 #include "document/format.h"
 #include "document/forms.h"
+#include "document/html/renderer.h" /* enum html_special_type */
 #include "intl/charsets.h" /* unicode_val_T */
 #include "util/align.h"
 #include "util/color.h"
@@ -18,7 +19,6 @@ struct menu_item;
 struct part;
 struct string;
 struct uri;
-enum html_special_type;
 
 /* XXX: This is just terible - this interface is from 75% only for other HTML
  * files - there's lack of any well defined interface and it's all randomly
