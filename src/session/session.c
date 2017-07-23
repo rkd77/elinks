f86be659718c0cd0a67f88b42f07044c23d0d028
Witold Filipczyk <witekfl@poczta.onet.pl>
2017-07-23 19:50:15 +0200

    Retry only for https protocol

--- src/session/session.c
+++ src/session/session.c
@@ -313,7 +313,7 @@ print_error_dialog(struct session *ses, struct connection_state state,
 
 	add_to_string(&msg, get_state_message(state, ses->tab->term));
 
-	if (!uri) {
+	if (!uri || uri->protocol != PROTOCOL_HTTPS) {
 		info_box(ses->tab->term, MSGBOX_FREE_TEXT,
 		N_("Error"), ALIGN_CENTER,
 		msg.source);
