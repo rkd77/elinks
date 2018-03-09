7a403ef735322713590865c10ff82700c7710ae2
Felix Janda <felix.janda@posteo.de>
2018-03-09 17:31:40 -0500

    Fix compatibility with Ruby >= 1.9
    Based on patch by Ian Abbott
    
    Bug: https://bugs.gentoo.org/424789

--- src/scripting/ruby/core.h
+++ src/scripting/ruby/core.h
@@ -7,6 +7,20 @@ struct session;
 
 #include <ruby.h>	/* for VALUE */
 
+#ifndef RSTRING_LEN
+#define RSTRING_LEN(string) (RSTRING(string)->len)
+#endif
+
+#ifndef RSTRING_PTR
+#define RSTRING_PTR(string) (RSTRING(string)->ptr)
+#endif
+
+#ifdef HAVE_RB_ERRINFO
+#define RB_ERRINFO (rb_errinfo())
+#else
+#define RB_ERRINFO (ruby_errinfo)
+#endif
+
 VALUE erb_module;
 
 void alert_ruby_error(struct session *ses, unsigned char *msg);
