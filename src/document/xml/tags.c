3af2c6f2d97682b92b3d7ac6b0576e22aef39c5a
Witold Filipczyk <witekfl@poczta.onet.pl>
2021-06-28 20:41:23 +0200

    [tables] Temporarily disable tables rendering

--- src/document/xml/tags.c
+++ src/document/xml/tags.c
@@ -3812,7 +3812,7 @@ tags_html_table(struct source_renderer *renderer, void *no, unsigned char *attr,
 {
 	struct html_context *html_context = renderer->html_context;
 	xmlpp::Element *node = no;
-	if (html_context->options->tables
+	if (false && html_context->options->tables
 	    && html_context->table_level < HTML_MAX_TABLE_LEVEL) {
 		format_table(attr, html, eof, end, html_context);
 		ln_break(html_context, 2);
