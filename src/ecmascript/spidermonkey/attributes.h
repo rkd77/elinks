4a6786df9881bec362a899b7e17a1c90849de90e
Witold Filipczyk <witekfl@poczta.onet.pl>
2021-10-25 16:51:07 +0200

    [spidermonkey] attributes in separate file

---
+++ src/ecmascript/spidermonkey/attributes.h
@@ -0,0 +1,8 @@
+#ifndef EL__ECMASCRIPT_SPIDERMONKEY_ATTRIBUTES_H
+#define EL__ECMASCRIPT_SPIDERMONKEY_ATTRIBUTES_H
+
+#include "ecmascript/spidermonkey/util.h"
+
+JSObject *getAttributes(JSContext *ctx, void *node);
+
+#endif
