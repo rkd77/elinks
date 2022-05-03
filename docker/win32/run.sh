b2cfafef13354f6dbcee34e370e2aefa39dda475
Unknown <dev@null>
2022-05-03 20:45:07 +0200

    [ win ] mounting tmp to docker guest

--- docker/win32/run.sh
+++ docker/win32/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run -it \
   --name=elinks-win32-dev \
-  -v /home/miky:/home/miky \
+  -v /tmp:/tmp/host \
   elinks-win32-dev:latest \
   /bin/bash
--- docker/win64/run.sh
+++ docker/win64/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run -it \
   --name=elinks \
+  -v /tmp:/tmp/host \
   elinks-dev:latest \
   /bin/bash
