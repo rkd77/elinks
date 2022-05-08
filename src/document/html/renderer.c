rkd77/elinks

commit b4045ef864de2ba106cd26468876803cf1100064

[blockquote] Added condition for y. Refs #112

src/document/html/renderer.c
@@ -338,6 +338,10 @@ draw_blockquote_chars(struct part *part, int y, struct html_context *html_contex
 	int x = par_format.orig_leftmargin;
 	struct screen_char *const schar = get_format_screen_char(html_context, 0);
 
+	if (y > part->box.height) {
+		return;
+	}
+
 	schar->data = '>';
 	for (i = 1; i < par_format.blockquote_level; i++) {
 		copy_screen_chars(&POS(x, y), schar, 1);
@@ -383,7 +387,7 @@ expand_lines(struct html_context *html_context, struct part *part,
 	par_format.color.background = bgcolor;
 
 	for (line = 0; line < lines; line++) {
-		realloc_line(html_context, part->document, Y(y + line), X(x));
+		if (realloc_line(html_context, part->document, Y(y + line), X(x))) return;
 
 		if (par_format.blockquote_level) {
 			draw_blockquote_chars(part, y + line, html_context);

commit 027d0f9800989dc97097c69b537ff0e0e1226219

[expand] Do not return. Refs #167
This bug was introduced in commit b4045ef864de2ba106cd26468876803cf1100064.
I don't know whether it does not break blockquotes again. No test case.

src/document/html/renderer.c
@@ -362,7 +362,7 @@ expand_lines(struct html_context *html_context, struct part *part,
 	}
 
 	for (line = 0; line < lines; line++) {
-		if (realloc_line(html_context, part->document, Y(y + line), X(x))) return;
+		realloc_line(html_context, part->document, Y(y + line), X(x));
 
 		if (par_elformat.blockquote_level) {
 			draw_blockquote_chars(part, y + line, html_context);