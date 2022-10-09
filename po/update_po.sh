36b08e69d148d665827967c65df2b818b25b6d66
Witold Filipczyk <witekfl@poczta.onet.pl>
2022-10-09 17:24:08 +0200

    [po] meson target update-po

---
+++ po/update_po.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SRC=$(dirname -- ${BASH_SOURCE[0]})
+
+for po in "$SRC"/*.po
+do
+	lang=$(basename $po .po)
+	echo -n "$lang"
+	if $(msgmerge "$SRC/$lang.po" "$SRC/elinks.pot" -o "$lang.new.po"); then
+		mv -f "$lang.new.po" "$SRC/$lanf.po"
+	else
+		echo "msgmerge failed!"
+		rm -f "$lang.new.po"
+	fi
+done
