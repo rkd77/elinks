9563fef8420d8a1fce3e44ccf150c800c91337b3
Witold Filipczyk <witekfl@poczta.onet.pl>
2023-10-20 13:44:41 +0200

    [document] check also doc_view->vs

--- src/document/view.h
+++ src/document/view.h
@@ -33,12 +33,14 @@ struct document_view {
 
 #define get_old_current_link(doc_view) \
 	(((doc_view) \
+	  && (doc_view)->vs \
 	  && (doc_view)->vs->old_current_link >= 0 \
 	  && (doc_view)->vs->old_current_link < (doc_view)->document->nlinks) \
 	? &(doc_view)->document->links[(doc_view)->vs->old_current_link] : NULL)
 
 #define get_current_link(doc_view) \
 	(((doc_view) \
+	  && (doc_view)->vs \
 	  && (doc_view)->vs->current_link >= 0 \
 	  && (doc_view)->vs->current_link < (doc_view)->document->nlinks) \
 	? &(doc_view)->document->links[(doc_view)->vs->current_link] : NULL)
