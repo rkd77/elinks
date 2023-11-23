b6af8fd0b40df520dc6efe7f66185ca6fbb4a626
Witold Filipczyk <witekfl@poczta.onet.pl>
2023-11-23 13:59:17 +0100

    [docker] mujs updated to 1.3.4 in win-static builds

--- docker/win32-static/build_mujs.sh
+++ docker/win32-static/build_mujs.sh
@@ -1,7 +1,7 @@
 cd
-wget https://mujs.com/downloads/mujs-1.3.3.tar.gz
-rm -rf mujs-1.3.3
-tar -xf mujs-1.3.3.tar.gz
-cd mujs-1.3.3
+wget https://mujs.com/downloads/mujs-1.3.4.tar.gz
+rm -rf mujs-1.3.4
+tar -xf mujs-1.3.4.tar.gz
+cd mujs-1.3.4
 patch -p1 < ../mujs.diff
-make CC=i686-w64-mingw32-gcc AR=i686-w64-mingw32-ar prefix=$HOME install
+make CC=i686-w64-mingw32-gcc AR=i686-w64-mingw32-ar HAVE_READLINE=no prefix=$HOME install
