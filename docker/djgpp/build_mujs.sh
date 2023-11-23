42e2442d376073b6b12bdc2022bff417c713785a
Witold Filipczyk <witekfl@poczta.onet.pl>
2023-11-23 14:26:02 +0100

    [docker] Also upgrade mujs for dos

--- docker/djgpp/build_mujs.sh
+++ docker/djgpp/build_mujs.sh
@@ -1,7 +1,7 @@
 cd
-wget https://mujs.com/downloads/mujs-1.3.3.tar.gz
-rm -rf mujs-1.3.3
-tar -xf mujs-1.3.3.tar.gz
-cd mujs-1.3.3
+wget https://mujs.com/downloads/mujs-1.3.4.tar.gz
+rm -rf mujs-1.3.4
+tar -xf mujs-1.3.4.tar.gz
+cd mujs-1.3.4
 patch -p1 < ../mujs.diff
-make CC=i586-pc-msdosdjgpp-gcc AR=i586-pc-msdosdjgpp-ar CFLAGS="-Wno-error -DWATT32_NO_NAMESPACE -U__STRICT_ANSI__" prefix=$HOME install
+make CC=i586-pc-msdosdjgpp-gcc AR=i586-pc-msdosdjgpp-ar HAVE_READLINE=no CFLAGS="-Wno-error -DWATT32_NO_NAMESPACE -U__STRICT_ANSI__" prefix=$HOME install
