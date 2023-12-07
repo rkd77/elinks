18bd6f269d479f882d423e0c98b0c243fbb14e8d
Witold Filipczyk <witekfl@poczta.onet.pl>
2023-12-07 12:58:37 +0100

    [win32] Updated curl

--- docker/win32-static/unpack_curl.sh
+++ docker/win32-static/unpack_curl.sh
@@ -1,5 +1,6 @@
 cd
-wget https://curl.se/windows/dl-8.4.0_6/curl-8.4.0_6-win32-mingw.zip
-unzip curl-8.4.0_6-win32-mingw.zip
-cd curl-8.4.0_6-win32-mingw
+wget https://curl.se/windows/dl-8.5.0_1/curl-8.5.0_1-win32-mingw.zip
+rm -rf curl-8.5.0_1-win32-mingw
+unzip curl-8.5.0_1-win32-mingw.zip
+cd curl-8.5.0_1-win32-mingw
 cp -a * ~/
