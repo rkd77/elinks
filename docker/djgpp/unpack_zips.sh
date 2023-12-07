cb39af36eeb4e6dbe5bc45437fa81c15d0115c12
Witold Filipczyk <witekfl@poczta.onet.pl>
2023-12-07 12:20:51 +0100

    [docker] Update for djgpp, binary archive for curl

--- docker/djgpp/unpack_zips.sh
+++ docker/djgpp/unpack_zips.sh
@@ -3,7 +3,7 @@ mkdir -p lib/pkgconfig
 mkdir zip
 cd zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2tk/expat20br2.zip
-wget http://ftp.delorie.com/pub/djgpp/current/v2tk/wat3211b.zip
+#wget http://ftp.delorie.com/pub/djgpp/current/v2tk/wat3211b.zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2tk/zlb13b.zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2apps/xz-525a.zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2apps/bz2-108a.zip
@@ -12,9 +12,10 @@ wget http://ftp.delorie.com/pub/djgpp/current/v2gnu/licv116b.zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2gnu/lus0910b.zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2tk/lua522b.zip
 wget http://ftp.delorie.com/pub/djgpp/current/v2gnu/rdln80b.zip
+wget http://mik.dyndns.pro/dos-stuff/bin/misc-dev.7z
 
 mkdir tmp; unzip expat20br2.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
-mkdir tmp; unzip wat3211b.zip -d tmp; mv -f tmp/net/watt/inc tmp/net/watt/include; cp -a tmp/net/watt/include $HOME/; cp -a tmp/net/watt/lib $HOME/; rm -rf tmp
+#mkdir tmp; unzip wat3211b.zip -d tmp; mv -f tmp/net/watt/inc tmp/net/watt/include; cp -a tmp/net/watt/include $HOME/; cp -a tmp/net/watt/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip zlb13b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip xz-525a.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip bz2-108a.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
@@ -23,6 +24,11 @@ cp -a tmp/tmp2/sqlite3.pc $HOME/lib/pkgconfig/; rm -rf tmp
 mkdir tmp; unzip licv116b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip lus0910b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip rdln80b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
+mkdir tmp; cd tmp; 7za x -y ../misc-dev.7z; cd ..; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
+
+# remove to not break compilation
+rm -rf $HOME/include/winsock2.h $HOME/include/ws2tcpip.h
+
 sed -i -e 's|/dev/env/DJDIR|/home/elinks|g' $HOME/lib/pkgconfig/*.pc
 sed -i -e 's|/dev/env/DJDIR|/home/elinks|g' $HOME/lib/*.la
 sed -i -e 's/Libs\.private/#Libs.private/' $HOME/lib/pkgconfig/sqlite3.pc
