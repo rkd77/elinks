2c7072e0584d0ec09911a027339bc9c7fcf47724
Witold Filipczyk <witekfl@poczta.onet.pl>
2023-12-27 17:58:15 +0100

    [docker] Updated quickjs to 2023-12-09

--- docker/djgpp/build_quickjs.sh
+++ docker/djgpp/build_quickjs.sh
@@ -1,8 +1,8 @@
 cd
-rm -rf quickjs-2021-03-27
-wget https://bellard.org/quickjs/quickjs-2021-03-27.tar.xz
-tar xf quickjs-2021-03-27.tar.xz
-cd quickjs-2021-03-27
+rm -rf quickjs-2023-12-09
+wget https://bellard.org/quickjs/quickjs-2023-12-09.tar.xz
+tar xf quickjs-2023-12-09.tar.xz
+cd quickjs-2023-12-09
 patch -p1 < ../quickjs-dos.diff
 make -f Makefile.dos prefix=$HOME
 make -f Makefile.dos install prefix=$HOME
