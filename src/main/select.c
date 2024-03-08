ce13ef6f97e9c30e1327de2595ba72d7ef5c0629
Witold Filipczyk <witekfl@poczta.onet.pl>
2024-03-08 13:33:58 +0100

    [event] Do not mix libev headers with libevent

--- src/main/select.c
+++ src/main/select.c
@@ -37,7 +37,7 @@
 #define USE_LIBEVENT
 #endif
 
-#if (defined(HAVE_EVENT2_EVENT_H) || defined(HAVE_EV_EVENT_H) || defined(HAVE_LIBEV_EVENT_H)) && defined(HAVE_LIBEVENT) && !defined(OPENVMS) && !defined(DOS)
+#if (defined(HAVE_EVENT2_EVENT_H) || defined(HAVE_EV_EVENT_H)) && defined(HAVE_LIBEVENT) && !defined(OPENVMS) && !defined(DOS)
 #if defined(HAVE_EVENT2_EVENT_H)
 #include <event2/event.h>
 #elif defined(HAVE_EV_EVENT_H)
