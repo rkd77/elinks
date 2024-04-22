af20c0e7e80b4b741d3c061fba7798d940d56ced
Witold Filipczyk <witekfl@poczta.onet.pl>
2024-04-22 20:59:30 +0200

    [css] execute old css code, when libcss is disabled, but compiled-in

--- src/document/css/apply.c
+++ src/document/css/apply.c
@@ -236,7 +236,9 @@ examine_element(struct html_context *html_context, struct css_selector *base,
 
 #ifdef CONFIG_CSS
 #ifdef CONFIG_LIBCSS
-#else
+	if (!html_context->options->libcss_enable)
+#endif
+	{
 	if (element->attr.class_ && seltype <= CST_CLASS) {
 		const char *class_ = element->attr.class_;
 
@@ -258,7 +260,7 @@ examine_element(struct html_context *html_context, struct css_selector *base,
 		selector = find_css_selector(selectors, CST_ID, rel, element->attr.id, -1);
 		process_found_selector(selector, CST_ID, base);
 	}
-#endif
+	}
 #endif
 
 #undef process_found_selector
