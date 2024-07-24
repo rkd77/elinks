157095513720deaa4b31e294c440ec4c0872d0c0
Witold Filipczyk <witekfl@poczta.onet.pl>
2024-07-24 20:26:13 +0200

    [quickjs] parentNode, parentElement .html
    They pass test with --leak-check=full

--- src/ecmascript/quickjs/element.c
+++ src/ecmascript/quickjs/element.c
@@ -1211,9 +1211,10 @@ js_element_get_property_parentElement(JSContext *ctx, JSValueConst this_val)
 		//dom_node_unref(el);
 		return JS_NULL;
 	}
-	//dom_node_unref(el);
+	JSValue rr = getElement(ctx, node);
+	dom_node_unref(node);
 
-	return getElement(ctx, node);
+	return rr;
 }
 
 static JSValue
@@ -1237,9 +1238,10 @@ js_element_get_property_parentNode(JSContext *ctx, JSValueConst this_val)
 		//dom_node_unref(el);
 		return JS_NULL;
 	}
-	//dom_node_unref(el);
+	JSValue rr = getElement(ctx, node);
+	dom_node_unref(node);
 
-	return getElement(ctx, node);
+	return rr;
 }
 
 static JSValue
