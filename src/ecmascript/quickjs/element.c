daa753e98a99d3955823ca1b750e50bd9ea79287
Witold Filipczyk <witekfl@poczta.onet.pl>
2024-07-24 20:18:47 +0200

    [quickjs] element.previousSibling

--- src/ecmascript/quickjs/element.c
+++ src/ecmascript/quickjs/element.c
@@ -1308,8 +1308,10 @@ js_element_get_property_previousSibling(JSContext *ctx, JSValueConst this_val)
 		return JS_NULL;
 	}
 	//dom_node_unref(el);
+	JSValue rr = getElement(ctx, node);
+	dom_node_unref(node);
 
-	return getElement(ctx, node);
+	return rr;
 }
 
 static JSValue
