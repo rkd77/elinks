22a72290ac41d27238a8861eea60b5cf3878b94b
Felix Janda <felix.janda@posteo.de>
2025-01-18 15:50:32 -0500

    [sftp] add declaration of ftp_curl_handle_error
    fixes compilation error when sftp is enabled but ftp is disabled

--- src/protocol/curl/sftp.h
+++ src/protocol/curl/sftp.h
@@ -12,6 +12,7 @@ extern struct module sftp_protocol_module;
 
 #if defined(CONFIG_SFTP) && defined(CONFIG_LIBCURL)
 extern protocol_handler_T sftp_protocol_handler;
+void ftp_curl_handle_error(struct connection *conn, CURLcode res);
 #else
 #define sftp_protocol_handler NULL
 #endif
