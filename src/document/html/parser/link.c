639073f78d0a0e0e00614f985ee7a066a4c259c4
Witold Filipczyk <witekfl@poczta.onet.pl>
2025-02-01 10:37:21 +0100

    [img] casts

--- src/document/html/parser/link.c
+++ src/document/html/parser/link.c
@@ -259,13 +259,13 @@ html_img_sixel(struct html_context *html_context, char *a,
 
 					if (fragment) {
 						if (cached->sixel) {
-							data = memacpy(fragment->data, fragment->length);
+							data = (unsigned char *)memacpy(fragment->data, fragment->length);
 							datalen = fragment->length;
 						} else {
 							data = el_sixel_get_image(fragment->data, fragment->length, &datalen);
 
 							if (data) {
-								(void)add_fragment(cached, 0, data, datalen);
+								(void)add_fragment(cached, 0, (const char *)data, datalen);
 								normalize_cache_entry(cached, datalen);
 								cached->sixel = 1;
 							}
