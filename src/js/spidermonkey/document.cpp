8e49b012411bf819b0ff5189a2e58703d250cf64
Witold Filipczyk <witekfl@poczta.onet.pl>
2025-03-28 12:36:34 +0100

    [spidermonkey] Another dom_html_element_get_tag_type

--- src/js/spidermonkey/document.cpp
+++ src/js/spidermonkey/document.cpp
@@ -473,15 +473,14 @@ document_get_property_currentScript(JSContext *ctx, unsigned int argc, JS::Value
 		dom_node *elem = (dom_node *)find_in_map(mapa, interpreter->element_offset);
 
 		if (elem) {
-			dom_string *tag_name = NULL;
-			dom_exception exc = dom_node_get_node_name(elem, &tag_name);
+			dom_html_element_type ty;
+			dom_exception exc = dom_html_element_get_tag_type(elem, &ty);
 
-			if (exc != DOM_NO_ERR || !tag_name) {
+			if (exc != DOM_NO_ERR) {
 				args.rval().setNull();
 				return true;
 			}
-			bool isScript = !strcmp("SCRIPT", dom_string_data(tag_name));
-			dom_string_unref(tag_name);
+			bool isScript = (ty == DOM_HTML_ELEMENT_TYPE_SCRIPT);
 
 			if (isScript) {
 				JSObject *obj = getNode(ctx, elem);
