4a237395bc98af8678d2aacb7a5465c49ae9b8af
Witold Filipczyk <witekfl@poczta.onet.pl>
2025-04-06 21:30:30 +0200

    [quickjs] window.scrollTo(x, y)

--- src/js/quickjs/window.c
+++ src/js/quickjs/window.c
@@ -348,6 +348,38 @@ js_window_scrollByPages(JSContext *ctx, JSValueConst this_val, int argc, JSValue
 	return JS_UNDEFINED;
 }
 
+/* @window_funcs{"scrollTo"} */
+JSValue
+js_window_scrollTo(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+#ifdef ECMASCRIPT_DEBUG
+	fprintf(stderr, "%s:%s\n", __FILE__, __FUNCTION__);
+#endif
+	REF_JS(this_val);
+	struct ecmascript_interpreter *interpreter = (struct ecmascript_interpreter *)JS_GetContextOpaque(ctx);
+	struct view_state *vs = interpreter->vs;
+	struct document_view *doc_view = vs->doc_view;
+	struct session *ses = doc_view->session;
+	struct terminal *term = ses->tab->term;
+
+	if (argc == 2) {
+		int x = 0;
+		int y = 0;
+
+		JS_ToInt32(ctx, &x, argv[0]);
+		JS_ToInt32(ctx, &y, argv[1]);
+
+		int sx = (x + term->cell_width - 1) / term->cell_width;
+		int sy = (y + term->cell_height - 1) / term->cell_height;
+
+		vertical_scroll(ses, doc_view, sy - doc_view->vs->y);
+		horizontal_scroll(ses, doc_view, sx - doc_view->vs->x);
+	}
+
+	return JS_UNDEFINED;
+}
+
+
 /* @window_funcs{"setInterval"} */
 JSValue
 js_window_setInterval(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
@@ -1083,6 +1115,7 @@ static const JSCFunctionListEntry js_window_proto_funcs[] = {
 	JS_CFUNC_DEF("scrollBy", 2, js_window_scrollBy),
 	JS_CFUNC_DEF("scrollByLines", 1, js_window_scrollByLines),
 	JS_CFUNC_DEF("scrollByPages", 1, js_window_scrollByPages),
+	JS_CFUNC_DEF("scrollTo", 2, js_window_scrollTo),
 	JS_CFUNC_DEF("setInterval", 2, js_window_setInterval),
 	JS_CFUNC_DEF("setTimeout", 2, js_window_setTimeout),
 	JS_CFUNC_DEF("toString", 0, js_window_toString)
