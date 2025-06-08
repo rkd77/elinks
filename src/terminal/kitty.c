f59be954fa94e83cd0c5b3570418af1558d1cd2c
Witold Filipczyk <witekfl@poczta.onet.pl>
2025-06-08 16:36:29 +0200

    [kitty] missing files

---
+++ src/terminal/kitty.c
@@ -0,0 +1,202 @@
+/* Terminal kitty routines. */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "elinks.h"
+
+#include "document/document.h"
+#include "osdep/osdep.h"
+#include "terminal/hardio.h"
+#include "terminal/kitty.h"
+#include "terminal/screen.h"
+#include "terminal/terminal.h"
+#include "util/base64.h"
+#include "util/memcount.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "terminal/stb_image.h"
+
+unsigned char *
+el_kitty_get_image(char *data, int length, int *outlen, int *width, int *height)
+{
+	ELOG
+	int comp;
+	unsigned char *pixels = stbi_load_from_memory((unsigned char *)data, length, width, height, &comp, 4);
+	unsigned char *b64;
+
+	if (!pixels) {
+		return NULL;
+	}
+	int size = *width * *height * 4;
+
+	b64 = base64_encode_bin(pixels, size, outlen);
+	stbi_image_free(pixels);
+
+	return b64;
+}
+
+int
+add_kitty_image_to_document(struct document *doc, const char *data, int datalen, int lineno, struct k_image **imagine, int width, int height)
+{
+	ELOG
+	struct k_image *im = mem_calloc(1, sizeof(*im));
+
+	if (imagine) {
+		*imagine = NULL;
+	}
+
+	if (!im) {
+		return 0;
+	}
+
+	if (!init_string(&im->pixels)) {
+		mem_free(im);
+		return 0;
+	}
+	im->cy = lineno;
+	im->cx = 0;
+	im->width = width;
+	im->height = height;
+	add_bytes_to_string(&im->pixels, data, datalen);
+
+	int ile = (height + doc->options.cell_height - 1) / doc->options.cell_height;
+	add_to_list(doc->k_images, im);
+
+	if (imagine) {
+		*imagine = im;
+	}
+
+	return ile;
+}
+
+void
+delete_k_image(struct k_image *im)
+{
+	ELOG
+	del_from_list(im);
+	done_string(&im->pixels);
+	mem_free(im);
+}
+
+void
+try_to_draw_k_images(struct terminal *term)
+{
+	ELOG
+
+	if (!term->kitty || !term->number_of_images) {
+		return;
+	}
+	struct string text;
+
+	if (!init_string(&text)) {
+		return;
+	}
+	add_to_string(&text, "\033_Ga=d\033\\");
+
+	if (text.length) {
+		if (term->master) want_draw();
+		hard_write(term->fdout, text.source, text.length);
+		if (term->master) done_draw();
+	}
+	done_string(&text);
+
+	int i;
+
+	for (i = 0; i < term->number_of_images; i++) {
+		struct k_image *im = term->k_images[i];
+
+		if (!init_string(&text)) {
+			return;
+		}
+		add_cursor_move_to_string(&text, im->cy + 1, im->cx + 1);
+
+		if (!im->sent) {
+			int m;
+			int left = im->pixels.length;
+			int sent = 0;
+			while (1) {
+				m = left >= 4000;
+				add_format_to_string(&text, "\033_Gf=32,i=%d,s=%d,v=%d,m=%d,t=d,q=1;", im->number, im->width, im->height, m);
+				add_bytes_to_string(&text, im->pixels.source + sent, m ? 4000 : left);
+				add_to_string(&text, "\033\\");
+				if (!m) {
+					break;
+				}
+				sent += 4000;
+				left -= 4000;
+			};
+			im->sent = 1;
+		}
+		add_format_to_string(&text, "\033_Gi=%d,x=%d,y=%d,w=%d,h=%d,a=p,q=1\033\\", im->number, im->x, im->y, im->w, im->h);
+
+		if (text.length) {
+			if (term->master) want_draw();
+			hard_write(term->fdout, text.source, text.length);
+			if (term->master) done_draw();
+		}
+		done_string(&text);
+	}
+}
+
+struct k_image *
+copy_k_frame(struct k_image *src, struct el_box *box, int cell_width, int cell_height, int dx, int dy)
+{
+	ELOG
+	struct k_image *dest = mem_calloc(1, sizeof(*dest));
+
+	if (!dest) {
+		return NULL;
+	}
+
+	if (!init_string(&dest->pixels)) {
+		mem_free(dest);
+		return NULL;
+	}
+	add_string_to_string(&dest->pixels, &src->pixels);
+
+	int cx = src->cx - dx;
+	int cy = src->cy - dy;
+
+	int clipx = cx >= 0 ? 0 : (-cx * cell_width);
+	int clipy = cy >= 0 ? 0 : (-cy * cell_height);
+
+	int clipwidth = box->width * cell_width;
+	int clipheight = box->height * cell_height;
+
+	if (src->width < clipwidth) {
+		clipwidth = src->width;
+	}
+	if (src->height < clipheight) {
+		clipheight = src->height;
+	}
+
+	if (cx * cell_width + clipwidth >= box->width * cell_width) {
+		clipwidth = (box->width * cell_width - cx * cell_width);
+	}
+	if (cy * cell_height + clipheight >= box->height * cell_height) {
+		clipheight = (box->height * cell_height - cy * cell_height);
+	}
+	dest->x = clipx;
+	dest->y = clipy;
+	dest->w = clipwidth;
+	dest->h = clipheight;
+
+	dest->cx = cx < 0 ? 0 : cx;
+	dest->cy = cy < 0 ? 0 : cy;
+	dest->width = src->width;
+	dest->height = src->height;
+
+	dest->id = src->id;
+	dest->number = src->number;
+	dest->sent = src->sent;
+
+	return dest;
+}
---
+++ src/terminal/kitty.h
@@ -0,0 +1,49 @@
+#ifndef EL__TERMINAL_KITTY_H
+#define EL__TERMINAL_KITTY_H
+
+#include "util/lists.h"
+#include "util/string.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef CONFIG_KITTY
+struct document;
+struct el_box;
+struct terminal;
+
+struct k_image {
+	LIST_HEAD_EL(struct k_image);
+	struct string pixels;
+	int cx;
+	int cy;
+	int width;
+	int height;
+	int id;
+	int number;
+	int x;
+	int y;
+	int w;
+	int h;
+	unsigned int sent:1;
+};
+
+void delete_k_image(struct k_image *im);
+
+void try_to_draw_k_images(struct terminal *term);
+
+/* return height of image in terminal lines */
+int add_kitty_image_to_document(struct document *doc, const char *data, int datalen, int lineno, struct k_image **imagine, int width, int height);
+
+struct k_image *copy_k_frame(struct k_image *src, struct el_box *box, int cell_width, int cell_height, int dx, int dy);
+
+unsigned char *el_kitty_get_image(char *data, int len, int *outlen, int *width, int *height);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EL__TERMINAL_KITTY_H */
