2597e048ab0abe09313ac442e1609a42221e6d3d
Witold Filipczyk <witekfl@poczta.onet.pl>
2025-11-22 16:15:27 +0100

    [djgpp] download gettext
    System gettext will allow faster rebuilds.

--- docker/djgpp/unpack_zips.sh
+++ docker/djgpp/unpack_zips.sh
@@ -12,6 +12,7 @@ wget http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/v2gnu/
 wget http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/v2gnu/lus0910b.zip
 wget http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/v2tk/lua522b.zip
 wget http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/v2gnu/rdln80b.zip
+wget http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/v2gnu/gtxt201b.zip
 wget http://mik.dyndns.pro/dos-stuff/bin/misc-dev.7z
 
 mkdir tmp; unzip expat20br2.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
@@ -23,6 +24,7 @@ cp -a tmp/tmp2/sqlite3.pc $HOME/lib/pkgconfig/; rm -rf tmp
 mkdir tmp; unzip licv116b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip lus0910b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip rdln80b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
+mkdir tmp; unzip gtxt201b.zip -d tmp; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; cd tmp; 7za x -y ../misc-dev.7z; cd ..; cp -a tmp/include $HOME/; cp -a tmp/lib $HOME/; rm -rf tmp
 mkdir tmp; unzip wat3211b.zip -d tmp; mv -f tmp/net/watt/inc tmp/net/watt/include; cp -a tmp/net/watt/include $HOME/; cp -a tmp/net/watt/lib $HOME/; rm -rf tmp
 
